b89ed7c45e5e638c79ac63df440b50aca47fa1cf
Eldar Abibulaev <abibulaev.eldar@gmail.com>
2021-09-27 14:36:02 +0300

    PR2 -> Done

--- P2/practice2.py
+++ P2/practice2.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 
 
 def k1(x1, x2):
-    return np.sqrt((x1[0] - x2[0]) ** 2 + (x1[1] - x2[1]) ** 2)
+    return (x1[0] - x2[0]) ** 2 + (x1[1] - x2[1]) ** 2
     pass
 
 
@@ -15,6 +15,7 @@ for x in X:
     plt.plot(x[0], x[1], "o")
 plt.xlim(0, 5)
 plt.ylim(0, 5)
+plt.grid()
 plt.savefig("test.png")
 plt.close()
 
@@ -36,4 +37,37 @@ cov_matrix = np.cov(np.array([x[0] for x in D]), np.array([x[1] for x in D]))
 print(cov_matrix)
 for x in D:
     plt.plot(x[0], x[1], "o")
-    plt.savefig("test2.png")
+plt.xlim(-12, 12)
+plt.grid()
+plt.savefig("test2.png")
+plt.close()
+
+print("Eigenvalues", end=" ")
+print(str(np.linalg.eigvals(cov_matrix)))
+
+print("Corr matrix")
+print(np.corrcoef(np.array([x[0] for x in D]), np.array([x[1] for x in D])))
+
+# part 3
+print("Part 3")
+X = D - np.mean(D, 0)
+print(X)
+for x in X:
+    plt.plot(x[0], x[1], "o")
+plt.xlim(-12, 12)
+plt.grid()
+plt.savefig("test3.png")
+plt.close()
+
+eigen_values, eigen_vectors = np.linalg.eig(cov_matrix)
+print("Main components", end=" ")
+print(eigen_vectors)
+
+print("Residual variance", end=" ")
+print(eigen_values)
+
+print("First component projection", end=" ")
+print(np.dot(X, eigen_vectors[:, 0]))
+
+print("Second component projection", end=" ")
+print(np.dot(X, eigen_vectors[:, 1]))
